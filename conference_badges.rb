2ef6f5f4313af820abe84cc67717c41389e4319d
Alen Jo <alenjo874@gmail.com>
2022-02-22 10:36:41 -0500

    commit tree a079219f73fe4a3da88ea9c30a6754aa9dc5f695

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end   
+
+def batch_badge_creator(arr)
+ new_arr = arr.map do |name|
+    "Hello, my name is #{name}."
+ end
+ return  new_arr
+end
+
+def assign_rooms(arr)
+    room = 0
+    newer_arr = arr.map do |name|
+        "Hello, #{name}! You'll be assigned to room #{room += 1}!"
+     end
+     return  newer_arr
+end
+
+def printer(arr)
+    arr.each do |name|
+        puts "Hello, my name is #{name}."
+    end
+
+    room = 0
+    arr.each do |name|
+     puts   "Hello, #{name}! You'll be assigned to room #{room += 1}!"
+    end
+ 
+
+end
